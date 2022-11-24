f6995e96c17bed279f3d726ab698a96e5c6d1679
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-11-24 10:53:56 +0300

    Practice

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ function App() {
         <div>This is an example!</div>
         <div>This is an example!</div>
       </Container>
+      
       <Container header="Example header" textPosition="center">
         <img
           alt="turtle"
